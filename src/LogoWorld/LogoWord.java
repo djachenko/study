3b4a1bc63d7a5633180a4bb92f7826437f0fe331
djachenko <i.s.djachenko@gmail.com>
2013-03-21 00:45:01 +0700

    log4j added but not set.

--- src/LogoWorld/LogoWord.java
+++ src/LogoWorld/LogoWord.java
@@ -5,13 +5,23 @@ import LogoWorld.Commands.Command;
 import java.io.IOException;
 import java.util.Arrays;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 public class LogoWord
 {
+	static Logger logger = Logger.getLogger(LogoWord.class);
+
 	public static void main(String[] args) throws IOException
 	{
 		CommandFactory factory = new CommandFactory("input.txt");
 		Parser parser = new Parser(args[0]);
 		AP field = new AP();
+
+		BasicConfigurator.configure();
+
+		logger.info("Hello world!");
+		logger.error("Error!", new Exception("An exception"));
 
 		for ( ; parser.ready(); )
 		{
