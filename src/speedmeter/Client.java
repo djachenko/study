068223e6c70a640146ab940989f4bc8f659ce448
djachenko <i.s.djachenko@gmail.com>
2013-03-30 13:50:25 +0700

    Говнокод для соответсвия требованиям.

--- src/speedmeter/Client.java
+++ src/speedmeter/Client.java
@@ -8,46 +8,73 @@ import java.util.Scanner;
 
 public class Client
 {
+	private static final int BUFFERSIZE = 256 * 1024;
+	private static final int TRIES = 25;
+
 	public static void main(String[] args)
 	{
-		try (Socket socket = new Socket("localhost",7651))
+		try (Socket socket = new Socket("wsc203",7651))
 		{
 			Scanner input = new Scanner(socket.getInputStream());
 			OutputStream output = socket.getOutputStream();
 
-			byte [] buffer = new byte[1024];
+			byte [] buffer = new byte[BUFFERSIZE];
 
-			long start = System.currentTimeMillis();
+			long [] starts = new long[TRIES];
+			int [] nums = new int[TRIES];
+			long [] ends = new long[TRIES];
 
-			output.write(buffer);
-
-			int num;
-			long end;
-
-			if (input.hasNextInt())
+			for (int i = 0; i < TRIES; i++)
 			{
-				num = input.nextInt();
-			}
-			else
-			{
-				throw new Exception("Unable to read num");
+				System.out.print(i +"... ");
+
+				starts[i] = System.currentTimeMillis();
+
+				output.write(buffer);
+
+				System.out.println();
+
+				if (input.hasNextInt())
+				{
+					nums[i] = input.nextInt();
+					System.out.println(nums[i]);
+				}
+				else
+				{
+					throw new Exception("Unable to read num");
+				}
+
+				if (input.hasNextLong())
+				{
+					ends[i] = input.nextLong();
+				}
+				else
+				{
+				    throw new Exception("Unable to read end");
+				}
 			}
 
-			if (input.hasNextLong())
+			//System.out.println();
+
+			double averageNum = 0;
+			double averageTime = 0;
+
+			for (int i = 0; i < TRIES; i++)
 			{
-				end = input.nextLong();
-			}
-			else
-			{
-			    throw new Exception("Unable to read end");
+				averageNum += nums[i];
+				averageTime += ends[i] - starts[i];
 			}
 
-			double speed = (double)(end - start) / num;
+			averageNum /= TRIES;
+			averageTime /= TRIES;
 
-			System.out.println(num);
-			System.out.println(start);
-			System.out.println(end);
-			System.out.println(speed);
+
+			double speed = averageNum / averageTime;
+
+			System.out.println("Number of bytes: " + averageNum);
+			System.out.println("Transfer time: " + averageTime + " milliseconds");
+			System.out.println("Speed: " + speed + " bytes/millisecond");
+			System.out.println("Speed: " + speed / 1024 * 1000 + " Kbytes/second");
 		}
 		catch (UnknownHostException e)
 		{
@@ -63,6 +90,5 @@ public class Client
 
 			e.printStackTrace();
 		}
-
 	}
 }
--- src/speedmeter/Server.java
+++ src/speedmeter/Server.java
@@ -29,12 +29,12 @@ public class Server
 class ServerThread implements Runnable
 {
 	private Socket socket = null;
+	private static final int BUFFERSIZE = 256 * 1024;
+	private static final int TRIES = 25;
 
 	public ServerThread(Socket incoming)
 	{
 		socket = incoming;
-
-		System.out.println("ServerThread started");
 	}
 
 	@Override
@@ -42,17 +42,30 @@ class ServerThread implements Runnable
 	{
 		try
 		{
-			InputStream input =  socket.getInputStream();
+			System.out.println("ServerThread started");
 
+			InputStream input =  socket.getInputStream();
 			PrintStream output = new PrintStream(socket.getOutputStream());
 
-			byte [] buffer = new byte[1024];
+			byte [] buffer = new byte[BUFFERSIZE];
 
-			int size = input.read(buffer);
-			long end = System.currentTimeMillis();
+			for (int i = 0; i < TRIES; i++)
+			{
+				System.out.print(i + "... ");
 
-			output.println(size);
-			output.println(end);
+				int size = input.read(buffer);
+				long end = System.currentTimeMillis();
+
+				if (size == -1)
+				{
+					break;
+				}
+
+				System.out.println(size);
+
+				output.println(size);
+				output.println(end);
+			}
 		}
 		catch (IOException e)
 		{
