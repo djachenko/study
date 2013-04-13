d4c2dfda03142456b549c33a7001157802ad5dcc
djachenko <i.s.djachenko@gmail.com>
2013-04-13 13:22:51 +0700

    The very beginning of Pusher

---
+++ src/ru/nsu/djachenko/pusher/Field.java
@@ -0,0 +1,20 @@
+package ru.nsu.djachenko.pusher;
+
+public class Field
+{
+	private int height = 0;
+	private int width = 0;
+	private boolean [][] field = null;
+
+	public void init(String configFileName)
+	{
+	}
+
+	public boolean getCell(int x, int y)
+			throws
+	{
+		if
+
+		return field[y][x];
+	}
+}
---
+++ src/ru/nsu/djachenko/pusher/Pusher.java
@@ -0,0 +1,20 @@
+package ru.nsu.djachenko.pusher;
+
+public class Pusher
+{
+	private Field field = null;
+	private int x = -1;
+	private int y = -1;
+
+	public Pusher(Field iField, int iX, int iY)
+	{
+		this.field = iField;
+		this.x = iX;
+		this.y = iY;
+	}
+
+	public void moveLeft()
+	{
+
+	}
+}
