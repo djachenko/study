66018885240180be16a0dcd7bf4626eebea2b2be
justin <i.s.djachenko@gmail.com>
2014-09-28 01:53:58 +0700

    Fourth lab is finished.

--- task1/src/ru/nsu/fit/djachenko/javaperfomance/task1/lab4/Lab4.java
+++ task1/src/ru/nsu/fit/djachenko/javaperfomance/task1/lab4/Lab4.java
@@ -1,5 +1,9 @@
 package ru.nsu.fit.djachenko.javaperfomance.task1.lab4;
 
+import javassist.util.proxy.MethodHandler;
+import javassist.util.proxy.Proxy;
+import javassist.util.proxy.ProxyFactory;
+
 public class Lab4 {
 
 	static class Calculator {
@@ -8,15 +12,28 @@ public class Lab4 {
 		}
 	}
 
-	static Calculator createCalculator() throws Exception {
-		//todo use JavaAssist to inherit Calculator class,
-		//overload sum() method
-		//and add 1 to original return value, so 2 + 2 will be 5
-		return new Calculator();
+	private static Calculator createCalculator() throws Exception {
+        ProxyFactory proxyFactory = new ProxyFactory();
+
+        proxyFactory.setSuperclass(Calculator.class);
+        proxyFactory.setFilter(m -> m.getName().equals("sum"));
+
+        Class calculatorProxyClass = proxyFactory.createClass();
+
+        Calculator calculatorProxy = (Calculator)calculatorProxyClass.newInstance();
+
+        if (calculatorProxy != null) {
+            MethodHandler methodHandler = (self, m, proceed, args) -> (Integer)proceed.invoke(self, args) + 1;
+
+            ((Proxy)calculatorProxy).setHandler(methodHandler);
+        }
+
+        return calculatorProxy;
 	}
 
 	public static void main(String[] args) throws Exception {
 		Calculator cal = createCalculator();
+
 		System.out.println("2 + 2 = " + cal.sum(2, 2));
 	}
 }
