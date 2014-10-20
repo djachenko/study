abd22e9f63e3a6bb5eb4ad5c4907af2a2b2793d5
djachenko <i.s.djachenko@gmail.com>
2014-10-21 03:07:40 +0700

    Sixth lab implemented.

--- src/ru/nsu/fit/djachenko/javaperfomance/lab6/Lab6.java
+++ src/ru/nsu/fit/djachenko/javaperfomance/lab6/Lab6.java
@@ -2,41 +2,58 @@ package ru.nsu.fit.djachenko.javaperfomance.lab6;
 
 
 import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
-public class Lab6
+public class Lab6 extends ClassLoader
 {
-    public static void main(String args[]) {
-        String classDirectory = args[0];
-        /**
-         * Создаем загрузчик модулей.
-         */
-        //ModuleLoader loader = new ModuleLoader(classDirectory, ClassLoader.getSystemClassLoader());
+    public Class<?> loadClass(byte[] bytes)
+    {
+        return defineClass(null, bytes, 0, bytes.length);
+    }
 
-        /**
-         * Получаем список доступных модулей.
-         */
+    public static void main(String args[])
+    {
+        String classDirectory = "res/ru/nsu/fit/djachenko/javaperfomance/lab6/";
+
         File directory = new File(classDirectory);
         String[] classes = directory.list();
 
-        /**
-         * Загружаем и исполняем каждый модуль.
-         */
+        System.out.println(directory.getAbsolutePath());
 
-        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        Lab6 classLoader = new Lab6();
 
         for (String className: classes)
         {
-            if (className.endsWith(".class"))
+            try
             {
-              /*  try
-                {
-                    String moduleName = className.split(".class")[0];
+                byte[] bytes = Files.readAllBytes(Paths.get(classDirectory + className));
 
+                Class<?> loadedClass = classLoader.loadClass(bytes);
+
+                try
+                {
+                    Method getSecurityMessageMethod = loadedClass.getMethod("getSecurityMessage", null);
+
+                    Object object = loadedClass.newInstance();
+
+                    Object invokeResult = getSecurityMessageMethod.invoke(object);
+
+                    System.out.println(loadedClass.getName() + ", " + invokeResult);
                 }
-                catch (ClassNotFoundException | IllegalAccessException | InstantiationException e)
+                catch (NoSuchMethodException e)
+                {}
+                catch (InstantiationException | IllegalAccessException | InvocationTargetException e)
                 {
                     e.printStackTrace();
-                }*/
+                }
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
             }
         }
     }
